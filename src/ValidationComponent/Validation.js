272b5fec7de405294701718f0a445c92918f6e87
ssheaks <sarah.sheaks@gmail.com>
2020-11-13 15:00:30 -0800

    add input validation to Validation component

---
+++ src/ValidationComponent/Validation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const validation = (props) => {
+    console.log(props)
+    let validationText = <p></p>
+
+    if (props.length < 5) {
+        validationText = <p>Input is too short</p>
+    }
+
+    if (props.length > 20) {
+        validationText = <p>Input is too long</p>
+    }
+    return (
+        <div className="Validation">
+            {validationText}
+        </div>
+    )
+}
+
+export default validation;
