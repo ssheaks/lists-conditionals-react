6c9bc419381e64bc8d489bbdc5928da3dbee1c5c
ssheaks <sarah.sheaks@gmail.com>
2020-11-13 15:13:57 -0800

    add Char component and style

---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CharComponent.css'
+
+const Char = (props) => {
+    console.log(props)
+    return (
+        <div className="Char"></div>
+    )
+}
--- src/ValidationComponent/Validation.js
+++ src/ValidationComponent/Validation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Validation.css'
 
 const validation = (props) => {
     console.log(props)
